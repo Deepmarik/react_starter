31d702d82e7afd44371e2476a449be992a2ef1ab
Deepmarik <lukyanovateam@gmail.com>
2022-02-13 21:56:43 +0300

    create index.js, app.css

--- src/app.js
+++ src/app.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-class App extends React.Component {
+import { Button } from './components/Message/Button';
+function App() {
+    return (
+        <>
+            <h1>Welcome to REACT!</h1>
+            <Button />
+        </>
+    )
+}
+/*class App extends React.Component {
     render() {
         return (
+            <>
             <h1>Welcome to REACT!</h1>
+            <Button/>
+            </>
         )
     }
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
+}*/
+export default App;
+//ReactDOM.render(<App />, document.getElementById('root'));*/
